f8370b5278b81acd1446f8c0b4c199637f86fbd5
Daniel Hantoro <daniel.hantoro@silvertech.com>
2022-06-09 15:33:54 -0400

    update

--- src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -11,14 +11,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class PersistenceConfiguration {
     
-    // @Bean
-    // public DataSource dataSource() {
-    //     //Builder pattern
-    //     DataSourceBuilder builder = DataSourceBuilder.create();
-    //     builder.url("jdbc:postgresql://localhost:5432/conference_app");
-    //     builder.username("postgres");
-    //     builder.password("200799");
-    //     System.out.println("My custom datasource bean has been initialized and set");
-    //     return builder.build();
-    // }
+    @Bean
+    public DataSource dataSource() {
+        //Builder pattern
+        DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.url("jdbc:postgresql://localhost:5432/conference_app");
+        builder.username("postgres");
+        builder.password("200799");
+        System.out.println("My custom datasource bean has been initialized and set");
+        return builder.build();
+    }
 }
